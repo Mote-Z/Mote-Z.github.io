6503e5f35a48feae664c8f0969dbe0b51a7bd991
Mote <53243613+Mote-Z@users.noreply.github.com>
2024-08-15 23:14:05 +0800

    test raycaster

--- js/cube.js
+++ js/cube.js
@@ -30,10 +30,6 @@ class Loop {
   }
 }
 
-
-
-
-
 // init
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 70, 900 / 700, 0.1, 100 );
@@ -49,31 +45,36 @@ const mouse = new THREE.Vector2();
 // Handle mouse move event
 function onMouseMove(event) {
     event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-	console.log(event.clientX, window.innerWidth, mouse.x)
-    console.log(event.clientY, window.innerHeight, mouse.y)
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+	console.log(event.clientX, renderer.domElement.clientWidth, mouse.x)
+    console.log(event.clientY, renderer.domElement.clientHeight, mouse.y)
     raycaster.setFromCamera(mouse, camera);
 
-    const intersects = raycaster.intersectObjects(scene.children);
+    const intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects.length > 0) {
 		console.log(intersects)
 		intersects[0].object.material.color.set(this.color16())
-        const intersectedObject = intersects[0].object;
-        if (intersectedObject.userData) {
-            document.getElementById('infoCard').style.display = 'block';
-            document.getElementById('infoCard').innerHTML = `
-                <h3>${intersectedObject.userData.categories.join(', ')}</h3>
-                <p>Link: <a href="${intersectedObject.userData.link}" target="_blank">Open PDF</a></p>
-            `;
-        }
-    } else {
-        document.getElementById('infoCard').style.display = 'none';
     }
 }
 
 // Add event listeners
-window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('mousemove', onMouseMove);
+
+
+function createScene(name) {
+    const geometry = new THREE.BoxGeometry( Math.random() * 9, 3, 3 ); 
+    const material = new THREE.MeshNormalMaterial();
+    const cube = new THREE.Mesh(geometry, material);
+    cube.name = name;
+    cube.userData = {link: "https://mote-z.github.io/pdf/Defenit-CTF-2020.pdf", categories: ["a","b"]};
+    cube.tick = () => {
+        cube.rotation.z += 0.01;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    }
+    return cube;
+}
 
 //
 const menu = {
@@ -87,39 +88,24 @@ home: createScene('Home'),
 publications: createScene('Publications')
 };
 let currentScene = scenes.home;
-
-function createScene(name) {
-const geometry = new THREE.BoxGeometry( Math.random() * 9, 3, 3 ); 
-const material = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh(geometry, material);
-cube.name = name;
-cube.userData = {link: "https://mote-z.github.io/pdf/Defenit-CTF-2020.pdf", categories: ["a","b"]};
-cube.tick = () => {
-	cube.rotation.z += 0.01;
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-}
-return cube;
-}
-
 scene.add(currentScene);
 loop.render();
 loop.start();
 
 function switchScene(sceneName) {
-scene.remove(currentScene);
-currentScene = scenes[sceneName];
-scene.add(currentScene);
-loop.updatables = [];
-loop.updatables.push(currentScene);
-console.log(`Switched to ${sceneName}`);
+    scene.remove(currentScene);
+    currentScene = scenes[sceneName];
+    scene.add(currentScene);
+    loop.updatables = [];
+    loop.updatables.push(currentScene);
+    console.log(`Switched to ${sceneName}`);
 }
 
 // add event listener
 Object.keys(menu).forEach(key => {
-menu[key].addEventListener('click', () => {
-switchScene(key);
-});
+    menu[key].addEventListener('click', () => {
+        switchScene(key);
+    });
 });
 
 camera.position.z = 5;
